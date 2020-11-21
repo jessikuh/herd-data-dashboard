a660996564645883225d79a7d0e922ca375a51ea
Jessica Stamos <jessica@jessicastamos.com>
2020-11-20 22:56:33 -0600

    Componetized SVG; wrap all in if statement

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,14 @@
 <template>
-  <div>
-    <template v-if="pageMeta && pageMeta.sections">
-      <ul class="border-b-4 border-opacity-100 cursor-pointer font-bold flex tabs text-sm">
-        <li
-          v-for="({ label }, index) in pageMeta.sections" :key="index"
-          class="hover:text-gray-800 py-2 relative text-gray-500 uppercase"
-          :class="active === label ? 'active' : null"
-        >
-          <inner-tab :title="label" @selected="changeTab" />
-        </li>
-      </ul>
-    </template>
+  <div v-if="pageMeta && pageMeta.sections">
+    <ul class="border-b-4 border-opacity-100 cursor-pointer font-bold flex tabs text-sm">
+      <li
+        v-for="({ label }, index) in pageMeta.sections" :key="index"
+        class="hover:text-gray-800 py-2 relative text-gray-500 uppercase"
+        :class="active === label ? 'active' : null"
+      >
+        <inner-tab :title="label" @selected="changeTab" />
+      </li>
+    </ul>
 
     <div class="mt-6 text-gray-500">
       <data-headers :data="activeHeaders" />
@@ -22,7 +20,7 @@
       class="absolute bg-blue-400 hover:bg-blue-500 rounded right-0 top-2 py-1 px-2 text-white"
       @click="toggleConfigurations"
     >
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-settings"><circle cx="12" cy="12" r="3"/><path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"/></svg>
+      <cog-wheel />
     </button>
 
     <div class="threshold" v-if="showPanel">
@@ -41,6 +39,7 @@
 // eslint-disable-all
 import { fetchPageMeta, fetchItemMeta, fetchAnimalData } from '@/data';
 
+import CogWheel from '@/assets/CogWheel.vue';
 import ConfigurationPanel from '@/components/ConfigurationPanel.vue';
 import DataHeaders from '@/components/DataHeaders.vue';
 import DataList from '@/components/DataList.vue';
@@ -49,6 +48,7 @@ import Tab from '@/components/Tab.vue';
 export default {
   name: 'Home',
   components: {
+    'cog-wheel': CogWheel,
     'configuration-panel': ConfigurationPanel,
     'data-headers': DataHeaders,
     'data-list': DataList,
