a35241ea5707b0b1e6064f1d43879820074aa6d0
Jessica Stamos <jessica@jessicastamos.com>
2020-11-20 21:19:31 -0600

    Display tooltip information

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,9 +1,11 @@
 <template>
-  <div
-    class="bg-blue-400 h-5 inline-flex items-center info
-      justify-center ml-1 rounded-full text-sm text-white w-5"
+  <div class="inline-flex relative">
+    <div
+      class="bg-blue-400 cursor-pointer h-5 items-center info inline-flex
+        justify-center ml-1 rounded-full text-sm text-white w-5"
     >
       ?
+    </div>
   </div>
 </template>
 
@@ -12,6 +14,35 @@ export default {
   props: {
     data: {
       type: Object,
+    },
+  },
+  data() {
+    return {
+      open: false,
+    };
+  },
+  mounted() {
+    this.displayInfo();
+  },
+  methods: {
+    displayInfo() {
+      const el = this.$el;
+      const tooltip = document.createElement('div');
+      tooltip.classList = 'absolute bg-blue-400 leading-normal p-2 text-sm text-white w-80';
+
+      el.addEventListener('mousemove', (event) => {
+        tooltip.innerHTML = `<b>Short Name:</b> ${this.data.shortName}
+
+        <p><b>Description:</b> ${this.data.description}</p>`;
+        tooltip.style.left = `${event.pageX + 15}px`;
+        tooltip.style.top = `${event.pageY}px`;
+
+        document.body.appendChild(tooltip);
+      });
+
+      el.addEventListener('mouseout', () => {
+        document.body.removeChild(tooltip);
+      });
     },
   },
 };
