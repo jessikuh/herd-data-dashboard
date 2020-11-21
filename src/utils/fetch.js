07e6b2a59955ed09333674c3a22c46501490051d
Jessica Stamos <jessica@jessicastamos.com>
2020-11-20 22:21:58 -0600

    Trap for env URL having a trailing slash

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -2,8 +2,12 @@ const fetch = require('node-fetch');
 
 export default async function fetchAPI(path) {
   const base = process.env.VUE_APP_SAMPLE_API;
+  const lastChar = base.substr(-1);
+  let fetchURL = `${base}/${path}`;
 
-  return fetch(`${base}/${path}`)
+  if (lastChar === '/') fetchURL = `${base}${path}`;
+
+  return fetch(`${fetchURL}`)
     .then((res) => res.json())
     .then((json) => json)
     .catch((err) => {
